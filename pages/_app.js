082671d9c35be34a847efb0a114d5537ebfcb3ae
Michael Harmer <michaeltharmer@gmail.com>
2023-09-04 14:27:40 -0400

    Added vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/global.css";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { Analytics } from '@vercel/analytics';
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
       <Component {...pageProps} />
+      <Analytics />
     </UserProvider>
   );
 }
