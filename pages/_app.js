6f2a88f62b7a65e01e6c4af00e5ab61b65662fdc
Michael Harmer <michaeltharmer@gmail.com>
2023-09-04 14:29:53 -0400

    Resolve import error

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/global.css";
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import { Analytics } from '@vercel/analytics';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }) {
   return (
